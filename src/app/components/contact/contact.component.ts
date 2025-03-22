848cadcd8b15527933103a76aa709e97eab74edd
aysnmicah <132601404+aysnmicah@users.noreply.github.com>
2025-03-22 15:36:29 +0800

    contact

--- src/app/components/contact/contact.component.ts
+++ src/app/components/contact/contact.component.ts
@@ -1,11 +1,43 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-contact',
-  imports: [],
+  standalone: true,
+  imports: [CommonModule, FormsModule],
   templateUrl: './contact.component.html',
-  styleUrl: './contact.component.css'
+  styleUrls: ['./contact.component.css']
 })
 export class ContactComponent {
+  volunteerData = {
+    name: '',
+    interest: '',
+    message: '',
+    photo: null as File | null
+  };
 
+  reportData = {
+    type: '',
+    name: '',
+    email: '',
+    description: '',
+    image: null as File | null
+  };
+
+  onFileSelected(event: any) {
+    this.volunteerData.photo = event.target.files[0];
+  }
+
+  onReportFileSelected(event: any) {
+    this.reportData.image = event.target.files[0];
+  }
+
+  onSubmitVolunteer() {
+    alert('Volunteer form submitted (UI only)');
+  }
+
+  onSubmitReport() {
+    alert('Report form submitted (UI only)');
+  }
 }
