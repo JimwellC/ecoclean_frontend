4b60f24785131ded699cb52b29462c6798f74ed2
Jimwelll Calma <jimwelllcalma@Jimwellls-MacBook-Air.local>
2025-03-24 00:47:41 +0800

    Fix:about added and initial commit

--- src/app/components/about/about.component.ts
+++ src/app/components/about/about.component.ts
@@ -1,12 +1,13 @@
 import { Component, OnInit } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { CommonModule } from '@angular/common';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-about',
   templateUrl: './about.component.html',
   styleUrls: ['./about.component.css'],
-  imports: [CommonModule]
+  imports: [CommonModule, RouterModule]
 })
 export class AboutComponent implements OnInit {
   volunteers: any[] = [];
